bad7f9cad1ee7c127dc0e06f43c4beac90f10e2e
Altinisik nejdet <nejdet.altinisik@yahoo.fr>
2025-11-27 09:46:06 +0100

    Update Injecttodefendnotextrelecaseonralgo.h

--- Injecttodefendnotextrelecaseonralgo.h
+++ Injecttodefendnotextrelecaseonralgo.h
@@ -1,1 +1,9 @@
-
+//the suspicious behaviour dtarts with
+//long time nonattacl and long term
+//collaborative attackers self trust behzviour.
+//then, the clusters needed to predict attacks
+//must hold a topological esuation at the firdt lines of the algorithm in this file 
+//so, the chaotic systems are needed to extract our pc criticzl datas with
+//our own clusters that need to hold ui/ux models. this is two big steps
+//but we wont need the cloud because this is physically quantum possible.
+//the symetry comed from low treeholds in this environments.
